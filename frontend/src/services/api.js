c8ceed2a4914f9be64335fda4a065d82e4d6c831
Ivan Romero <222374809+IvanD-js@users.noreply.github.com>
2026-05-07 12:36:32 -0600

    update vercel.json and service front

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,20 @@
+// // frontend/src/services/api.js
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api/productos",
+//   timeout: 5000
+// });
+
+// export default api;
+
 // frontend/src/services/api.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api/productos",
-  timeout: 5000
+  // Si existe la variable de entorno la usa, si no, usa el servidor local
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  timeout: 10000 // Aumentamos un poco el tiempo por si el backend tarda en despertar
 });
 
 export default api;
